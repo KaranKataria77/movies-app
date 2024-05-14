2e331bb2c6cc3bbd7e97f669123a4abb8e8c24f7
Karan Kataria <karankataria771801@gmail.com>
2024-05-14 06:06:18 +0530

    eslibt error

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useRef } from "react";
 import Header from "../../components/Header";
 import Movies from "../../components/Movies";
